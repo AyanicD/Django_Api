c245b7571fb05d09b7fc9a3b0e9734722ebebe6c
AyanicD <ayan@attentive.ai>
2023-02-05 01:03:26 +0530

    Added History Features

--- device_api/models.py
+++ device_api/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from simple_history.models import HistoricalRecords
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    history = HistoricalRecords()
 
 class Device(models.Model):
     name = models.CharField(max_length=100,unique=True)
     type = models.CharField(max_length=25)
-    history = models.TextField(blank=True)
+    history = HistoricalRecords()
     timestamp = models.DateTimeField(default=timezone.now)
     employee = models.ForeignKey(Employee, blank=True, null=True, on_delete=models.SET_NULL)
