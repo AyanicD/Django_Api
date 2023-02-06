ae1e150cbb0b65883a607de950f028b8b57c70c9
AyanicD <ayan@attentive.ai>
2023-02-06 20:13:34 +0530

    History Admin Integration

--- device_api/admin.py
+++ device_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+from device_api.models import Device, Employee
 
-# Register your models here.
+admin.site.register(Device, SimpleHistoryAdmin)
+admin.site.register(Employee, SimpleHistoryAdmin)
